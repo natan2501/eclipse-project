171a6dbdebd9db9f50ec99498c3a2ebe1db0c12a
natan <natan@natanubuntu>
2019-08-06 01:10:55 +0300

    first commit

---
+++ Ebay/src/sample/com/PrrimitiveDataType.java
@@ -0,0 +1,22 @@
+package sample.com;
+
+public class PrrimitiveDataType {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		System.out.print("Hello world ןi am natan!!!");     
+
+		int age = 31;
+        System.out.println("my age is : "+ age);
+        double weight =71.5;
+        boolean male = true;
+        char letter = 'h';
+        System.out.println("my weight: " + weight);
+        System.out.println("male: " + male);
+        System.out.println("letter: " + letter);
+
+	}
+
+}
